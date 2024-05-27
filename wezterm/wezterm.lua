187f83c13de5916c69a9caadf086c9be6479eafd
Augustin <premieraugust28@gmail.com>
2024-05-27 08:52:16 +0000

    Add files via upload

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,5 @@
 local wezterm = require("wezterm")
-local theme = require("lua/rose_pine").moon
+local theme = require("lua/rose-pine").main
 
 local c = {}
 if wezterm.config_builder then
@@ -10,10 +10,7 @@ end
 -- COLORS
 c.use_fancy_tab_bar = false
 c.show_new_tab_button_in_tab_bar = false
-
--- c.colors = theme.colors()
-c.color_scheme = "tokyonight_storm"
-c.window_frame = theme.window_frame()
+c.color_scheme = "rose-pine"
 
 c.window_padding = {
 	left = 2,
@@ -21,37 +18,4 @@ c.window_padding = {
 	top = 0,
 	bottom = 0,
 }
-
-wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-	local palette = config.resolved_palette.tab_bar
-	local colors = {
-		bg = palette.background,
-		tab = tab.is_active and palette.active_tab.bg_color or palette.inactive_tab.bg_color,
-		fg = tab.is_active and palette.active_tab.fg_color or palette.inactive_tab.fg_color,
-	}
-
-	return {
-		{ Background = { Color = colors.bg } },
-		{ Foreground = { Color = colors.tab } },
-		{ Text = wezterm.nerdfonts.ple_lower_right_triangle },
-		{ Background = { Color = colors.tab } },
-		{ Foreground = { Color = colors.fg } },
-		{ Text = tab.active_pane.title },
-		{ Background = { Color = colors.tab } },
-		{ Foreground = { Color = colors.bg } },
-		{ Text = wezterm.nerdfonts.ple_upper_right_triangle },
-	}
-end)
-
--- OPEN NEW TERM IN SAME DIR
--- Function to get the working directory of the first terminal
-local function getInitialDir(window_config, pane_config, window, pane)
-	local pid = pane:pid()
-	if pid then
-		local dir = wezterm.get_child_process_cwd(pid)
-		return dir
-	end
-	return nil
-end
-
 return c
