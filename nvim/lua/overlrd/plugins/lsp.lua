3767ecadc89fdb03c20dfa119233a968709c2a1e
overlrd <premieraugust28@gmail.com>
2024-05-28 13:12:29 +0000

    edit: picom i3 nvim

--- nvim/lua/overlrd/plugins/lsp.lua
+++ nvim/lua/overlrd/plugins/lsp.lua
@@ -16,7 +16,21 @@ local servers = {
 				completion = {
 					callSnippet = "Replace",
 				},
-				diagnostics = { disable = { "missing-fields" } },
+				diagnostics = {
+					disable = { "missing-fields" },
+					globals = {
+						"vim",
+						"require",
+					},
+				},
+				workspace = {
+					-- Make the server aware of Neovim runtime files
+					library = vim.api.nvim_get_runtime_file("", true),
+				},
+				-- Do not send telemetry data containing a randomized but unique identifier
+				telemetry = {
+					enable = false,
+				},
 			},
 		},
 	},
